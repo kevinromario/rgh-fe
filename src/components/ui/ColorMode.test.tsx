2f2840f5ca2643477425a0aff25e17686d1ebee7
wiliromario <wiliromarioakukom@gmail.com>
2025-06-12 10:01:02 +0700

    test: refactor ColorMode.test.tsx

--- src/components/ui/ColorMode.test.tsx
+++ src/components/ui/ColorMode.test.tsx
@@ -6,11 +6,10 @@ import {
   LightMode,
   DarkMode,
 } from "./ColorMode";
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect, vi, beforeEach } from "vitest";
 import { useColorMode } from "../../hooks/useColorMode";
+import { renderWithProviders } from "src/test/utils/renderWithProviders";
 
-// Mock useColorMode hook
 vi.mock("../../hooks/useColorMode", () => {
   const state = { colorMode: "light", toggleColorMode: vi.fn() };
   return {
@@ -18,64 +17,50 @@ vi.mock("../../hooks/useColorMode", () => {
   };
 });
 
-describe("ColorModeProvider", () => {
-  it("renders ThemeProvider without crashing", () => {
-    render(<ColorModeProvider>Test Content</ColorModeProvider>);
-    expect(screen.getByText("Test Content")).toBeInTheDocument();
-  });
-});
-
-describe("ColorModeIcon", () => {
-  it("renders LuSun when in light mode", () => {
-    render(<ColorModeIcon />);
-    expect(screen.getByTestId("lu-sun")).toBeInTheDocument();
-  });
-});
-
-describe("ColorModeButton", () => {
-  it("renders button and icon", () => {
-    render(
-      <ChakraProvider value={defaultSystem}>
-        <ColorModeButton />
-      </ChakraProvider>
-    );
-    expect(screen.getByTestId("color-mode-btn")).toBeInTheDocument();
-    expect(screen.getByTestId("lu-sun")).toBeInTheDocument();
+describe("ColorMode components", () => {
+  describe("<ColorModeProvider />", () => {
+    it("renders children", () => {
+      render(<ColorModeProvider>Test Content</ColorModeProvider>);
+      expect(screen.getByText("Test Content")).toBeInTheDocument();
+    });
   });
 
-  it("calls toggleColorMode when clicked", () => {
-    render(
-      <ChakraProvider value={defaultSystem}>
-        <ColorModeButton />
-      </ChakraProvider>
-    );
-
-    const button = screen.getByTestId("color-mode-btn");
-    fireEvent.click(button);
-    expect(useColorMode().toggleColorMode).toHaveBeenCalled();
+  describe("<ColorModeIcon />", () => {
+    it("renders LuSun when in light mode", () => {
+      render(<ColorModeIcon />);
+      expect(screen.getByTestId("lu-sun")).toBeInTheDocument();
+    });
   });
-});
 
-describe("LightMode", () => {
-  it("renders LightMode span", () => {
-    render(
-      <ChakraProvider value={defaultSystem}>
-        <LightMode>Light Content</LightMode>
-      </ChakraProvider>
-    );
-    const span = screen.getByText("Light Content");
-    expect(span).toHaveClass("chakra-theme light");
+  describe("<ColorModeButton />", () => {
+    beforeEach(() => {
+      renderWithProviders(<ColorModeButton />);
+    });
+
+    it("renders button and icon", () => {
+      expect(screen.getByTestId("color-mode-btn")).toBeInTheDocument();
+      expect(screen.getByTestId("lu-sun")).toBeInTheDocument();
+    });
+
+    it("calls toggleColorMode when clicked", () => {
+      fireEvent.click(screen.getByTestId("color-mode-btn"));
+      expect(useColorMode().toggleColorMode).toHaveBeenCalled();
+    });
   });
-});
 
-describe("DarkMode", () => {
-  it("renders DarkMode span", () => {
-    render(
-      <ChakraProvider value={defaultSystem}>
-        <DarkMode>Dark Content</DarkMode>
-      </ChakraProvider>
-    );
-    const span = screen.getByText("Dark Content");
-    expect(span).toHaveClass("chakra-theme dark");
+  describe("<LightMode />", () => {
+    it("applies light class", () => {
+      renderWithProviders(<LightMode>Light Content</LightMode>);
+      expect(screen.getByText("Light Content")).toHaveClass(
+        "chakra-theme light"
+      );
+    });
+  });
+
+  describe("<DarkMode />", () => {
+    it("applies dark class", () => {
+      renderWithProviders(<DarkMode>Dark Content</DarkMode>);
+      expect(screen.getByText("Dark Content")).toHaveClass("chakra-theme dark");
+    });
   });
 });
